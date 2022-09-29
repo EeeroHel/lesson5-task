e0850f553c5c3ffbb5202c7196e74292005978a6
EeeroHel <98892120+EeeroHel@users.noreply.github.com>
2022-09-29 10:34:22 +0300

    Week5 task
    Calling node from react task

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+app.use(cors())
+const PORT = 3001
+
+app.get("/", (req, res) => {
+    res.status(200).json({message: "node server is responding"})
+})
+
+app.listen(PORT,() => {
+console.log(`Server is running on port ${PORT}`);
+
+})
+
+
+
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+const URL = "http://localhost:3001/"
+
+function App() {
+  const [message, setMessage] = useState(" ")
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response) => {
+      setMessage(response.data.message);
+    }).catch((error) => {
+      setMessage(error);
+    })
+  }, []);
+  
+
+  return (
+    <div>
+       <h3>This message was retrieved from my NodeJs server</h3>      
+       <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
